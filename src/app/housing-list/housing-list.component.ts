6dbc89a7a25d11a25beaf34e84377acd1fdc9663
PurpleMongrel <thaispdf@gmail.com>
2022-07-07 20:40:29 -0400

    using input decorator to allow component to access housing list

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import {HousingLocation} from "../housing-location"
 
 @Component({
   selector: 'app-housing-list',
@@ -13,6 +14,11 @@ export class HousingListComponent implements OnInit {
   }
 
   searchHousingLocations(searchText: string) {
-    console.log(searchText) }
+    console.log(searchText)
+  }
+
+  @Input() locationList: HousingLocation[] = [];
+
+  
 
 }
